b64978430b593653595038bfc506d0517cd34964
yxx <3165346340@qq.com>
2019-01-19 07:38:20 +0800

    把本地工程添加到git

---
+++ firstgit/src/com/itheima/firstgit/firstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.firstgit;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+}
